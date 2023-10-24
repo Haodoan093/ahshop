5b31d2f38040bc23ce59db3062aeaa4612fbc4de
Haodoan093 <122719130+Haodoan093@users.noreply.github.com>
2023-10-24 12:56:14 +0700

    editVeiew

--- index.php
+++ index.php
@@ -23,6 +23,8 @@
             include("pages/header.php");
             include("pages/menu.php");
             include("pages/main.php");
+            include("pages/carousel.php");
+            
             include("pages/footer.php");
             ?>
         </div>
@@ -46,37 +48,11 @@
         });
     </script>
 </body>
-<!-- <script type="text/javascript">
-    $(document).ready(function() {
 
-        var back = $(".prev");
-        var next = $(".next");
-        var steps = $(".step");
-
-        next.bind("click", function() {
-            $.each(steps, function(i) {
-                if (!$(steps[i]).hasClass('current') && !$(steps[i]).hasClass('done')) {
-                    $(steps[i]).addClass('current');
-                    $(steps[i - 1]).removeClass('current').addClass('done');
-                    return false;
-                }
-            })
-        });
-        back.bind("click", function() {
-            $.each(steps, function(i) {
-                if ($(steps[i]).hasClass('done') && $(steps[i + 1]).hasClass('current')) {
-                    $(steps[i + 1]).removeClass('current');
-                    $(steps[i]).removeClass('done').addClass('current');
-                    return false;
-                }
-            })
-        });
-
-    })
-</script> -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
 <script src="https://www.paypal.com/sdk/js?client-id=ATf__K3JKfjMk1TCxvMnu3efCCHCPkVBPeuSlSdDCVEoYOoJWf6ZrU9DpM9uV9N8NXG4NM_W7lzi9WE8&currency=USD"></script>
 <script src="js/app.js"></script>
+
 
 </html>
 
