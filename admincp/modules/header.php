a0d14598b1692d6da8e3f548297ddb5401a8002f
Haodoan093 <122719130+Haodoan093@users.noreply.github.com>
2023-11-02 12:16:26 +0700

    assa

--- admincp/modules/header.php
+++ admincp/modules/header.php
@@ -1,11 +1,19 @@
+<style>
+  /* CSS cho form tìm kiếm */
+  .search-form {
+    margin-left: auto; /* Đặt margin-left thành auto để đẩy form về bên phải */
+    margin-right: 250px;
+  }
+</style>
+
 <nav class="navbar navbar-expand-lg bg-dark w-100" style="height: 78px; width:100%;background-color: #222; position: fixed;">
   <div class="container-fluid">
     <a class="navbar-brand" href="#" style="color:white;">AHSHOP</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-left: 900px">
-      <form class="d-flex" role="search" >
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <form class="d-flex search-form" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
--- pages/carousel.php
+++ pages/carousel.php
@@ -5,27 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    
+    <style>
+  /* CSS cho container chứa các bài viết */
+  .product-container {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: nowrap;
+    overflow-x: auto;
+  }
+
+  /* CSS cho mỗi bài viết */
+  .product-card {
+    width: calc(25% - 10px);
+    margin-right: 10px;
+  }
+
+  /* CSS cho thẻ a bên trong .product-card */
+  .product-card a {
+    color: #000;
+    text-decoration: none;
+  }
+
+  /* CSS cho thẻ h3 */
+  h3 {
+    text-align: center; /* Căn giữa theo chiều ngang */
+  }
+</style>
 </head>
 <body>
   
-<h3>TIN TỨC MỚI NHẤT</h3>
-<ul class="product_list">
 
-  <?php while ($row_bv = mysqli_fetch_array($query_bv)) {
-  ?>
-    <li>
-      <a href="index.php?quanly=baiviet&idbaiviet=<?php echo $row_bv['id_baiviet'] ?>">
-        <img src="admincp/modules/quanlybaiviet/uploads/<?php echo $row_bv['hinhanh'] ?>">
-        <p class="title_product"><?php echo $row_bv['tenbaiviet'] ?></p>
-
-      </a>
-    </li>
-
-  <?php }
-  ?>
-
-</ul>
 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel" style="width: 70%; margin: auto; margin-top: 3%; margin-bottom: 3%;">
   <div class="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -55,6 +64,32 @@
     <div style="width: 100px;">
         <img src="../images/Screenshot 2023-10-18 125927.png" alt="">
     </div>
+
+
+    <?php
+  $sql_bv = "SELECT * FROM tbl_baiviet WHERE tinhtrang=1 ORDER BY id_baiviet DESC";
+  $query_bv = mysqli_query($mysqli, $sql_bv);
+  ?>
+  <h3>LỜI BÀY TỎ TỪ GIÁ TRỊ ĐÍCH THỰC</h3>
+  <div class="product-container">
+    <?php $i = 0;
+    while ($row_bv = mysqli_fetch_array($query_bv)) {
+      if ($i < 4) {
+        $i++;
+    ?>
+        <div class="product-card card">
+          <a href="index.php?quanly=baiviet&idbaiviet=<?php echo $row_bv['id_baiviet'] ?>">
+            <img src="admincp/modules/quanlybaiviet/uploads/<?php echo $row_bv['hinhanh'] ?>" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $row_bv['tenbaiviet'] ?></h5>
+              <p class="card-text"><?php echo $row_bv['tomtat'] ?></p>
+            </div>
+          </a>
+        </div>
+      <?php }
+    }
+    ?>
+  </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
