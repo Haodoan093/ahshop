9e69c4d1b4b194de66c0a3bfc26a47243fb89f54
Haodoan093 <122719130+Haodoan093@users.noreply.github.com>
2023-11-02 10:53:32 +0700

    d

--- admincp/modules/quanlydonhang/indonhang.php
+++ admincp/modules/quanlydonhang/indonhang.php
@@ -8,20 +8,20 @@ $query_donhang = mysqli_query($mysqli,$sql_donhang);
 $pdf = new tFPDF();
 $pdf->AddPage();
 $pdf->AddFont('DejaVu','','DejaVuSansCondensed.ttf',true);
-$pdf->SetFont('DejaVu','',14);
+$pdf->SetFont('DejaVu','',12);
 
 $pdf->SetFillColor(193,229,252);
-$pdf->Write(10,'Don hang gom co:');
+$pdf->Write(10,'Thông tin đơn hàng:');
 	$pdf->Ln(10);
 
 	$width_cell=array(5,35,80,20,30,40);
 
 	$pdf->Cell($width_cell[0],10,'ID',1,0,'C',true);
-	$pdf->Cell($width_cell[1],10,'Ma hang',1,0,'C',true);
-	$pdf->Cell($width_cell[2],10,'Ten san pham',1,0,'C',true);
-	$pdf->Cell($width_cell[3],10,'So luong',1,0,'C',true); 
-	$pdf->Cell($width_cell[4],10,'Gia',1,0,'C',true);
-	$pdf->Cell($width_cell[5],10,'Tong tien',1,1,'C',true); 
+	$pdf->Cell($width_cell[1],10,'Mã hàng',1,0,'C',true);
+	$pdf->Cell($width_cell[2],10,'Tên sản phẩm',1,0,'C',true);
+	$pdf->Cell($width_cell[3],10,'Số lượng',1,0,'C',true); 
+	$pdf->Cell($width_cell[4],10,'Giá',1,0,'C',true);
+	$pdf->Cell($width_cell[5],10,'Tổng tiền',1,1,'C',true); 
 	$pdf->SetFillColor(235,236,236); 
 	$fill=false;
 	$i = 0;
@@ -36,7 +36,7 @@ $pdf->Write(10,'Don hang gom co:');
 	$fill = !$fill;
 
 	}
-	$pdf->Write(10,'Cam on ban da dat hang tai website cua chung toi.');
+	//$pdf->Write(10,'Cam on ban da dat hang tai website cua chung toi.');
 	$pdf->Ln(10);
 $pdf->Output();
 ?>
